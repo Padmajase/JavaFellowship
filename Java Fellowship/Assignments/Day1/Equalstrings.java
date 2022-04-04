c4872e6ee558a5113ca41031142d8d048526912e
Padmajase <padmajapawar7@gmail.com>
2022-04-03 23:07:16 -0700

    Adding program3 from day1 assignment

---
+++ Java Fellowship/Assignments/Day1/Equalstrings.java
@@ -0,0 +1,16 @@
+//1.3 Write a program to check two strings are equal or not
+class Equalstrings{
+	public static void main(String args[])
+	{
+		String str1="unnati";
+		String str2="unnati";
+		if(str1==str2)
+		{
+			System.out.println("string 1 and string 2 are equal");
+		}
+		else
+		{
+			System.out.println("string 1 and string 2 are not equal");
+		}
+	}	
+}
