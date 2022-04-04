603b3cc85f6090cab031824bb51c308e2ab1b1b5
Padmajase <padmajapawar7@gmail.com>
2022-04-04 12:18:58 -0700

    adding while loop program for sum of n numbers from Day2 assignment

---
+++ Java Fellowship/Assignments/Day2/Sumofnnumbers.java
@@ -0,0 +1,16 @@
+// while loop 1. Write a Program for the sum of n natural numbers eg. Input N=5 output 1+2+3+4+5
+import java.util.Scanner;
+class Sumofnnumbers{
+	public static void main(String args[])
+	{
+		int i,sum=0;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter number to get summation ");
+		int n=scan.nextInt();
+		for(i=1;i<=n;i++)
+		{
+			sum+=i;
+		}
+		System.out.println(" Sum of "+n+" numbers is "+sum);
+	}
+}
