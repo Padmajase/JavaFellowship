e0a5f8f52d4375d77703c3f2bb42bcb98ab65498
Padmajase <padmajapawar7@gmail.com>
2022-04-03 23:05:38 -0700

    Adding program2 from day1 assignment

---
+++ Java Fellowship/Assignments/Day1/Primitivedatavalue.java
@@ -0,0 +1,24 @@
+//1.2 Write a Java program to display the default value of all primitive data types of Java
+public class Primitivedatavalue{
+	static	int a;
+	static long b;
+	static float c;
+	static double d;
+	static short e;
+	static char f;
+	static boolean g;
+	static byte h;
+	public static void main(String args[])
+	{
+		
+		System.out.println("default integer value is "+a);
+		System.out.println("default long value is "+b);
+		System.out.println("default float value is "+c);
+		System.out.println("default double value is "+d);
+		System.out.println("default short value is "+e);
+		System.out.println("default character value is "+f);
+		System.out.println("default boolean value is "+g);
+		System.out.println("default byte value is "+h);
+		
+	}
+}
