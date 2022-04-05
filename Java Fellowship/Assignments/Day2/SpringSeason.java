1345fb98f34b5b196e23a68aaf749ce6ddf77608
Padmajase <padmajapawar7@gmail.com>
2022-04-05 13:50:41 -0700

    Adding SpringSeason.java program  from day 2 assignments

---
+++ Java Fellowship/Assignments/Day2/SpringSeason.java
@@ -0,0 +1,19 @@
+// operator program 2. Write a program SpringSeason.java that takes two int values m and d from the command line and prints true if day d of month m 
+//is between March 20 (m = 3, d=20) and June 20 (m = 6, d = 20), false otherwise
+
+import java.util.Scanner;
+
+class SpringSeason {
+	public static void main(String args[]) {
+	
+		int m = Integer.parseInt(args[0]);
+		int d  = Integer.parseInt(args[1]);
+
+		boolean isBetween =  (m == 3 && d >= 20 && d <= 31)
+                         || (m == 4 && d >=  1 && d <= 30)
+                         || (m == 5 && d >=  1 && d <= 31)
+                         || (m == 6 && d >=  1 && d <= 20);
+
+		System.out.println(isBetween);
+	} 
+}
